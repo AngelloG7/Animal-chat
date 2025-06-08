d39d066a5605ca5bbd229b86ab1b082ee851fd2d
Angel Caro Galé <123036439+AngelloG7@users.noreply.github.com>
2025-06-08 16:49:21 -0500

    Create Reaccion.java
    implements MensajeRepositoryPort: Implementa el puerto de persistencia de mensajes.
    save: Guarda un Mensaje convirtiéndolo a MensajeDocument y luego lo reconvierte a Mensaje para devolverlo.
    findRecentMessages: Utiliza el método de Spring Data para obtener mensajes recientes.
    searchMessages: Implementa la lógica de búsqueda, usando las @Query definidas en el repositorio de Spring Data.
    deleteExpiredMessages: Se ha comentado que, gracias a @Indexed(expireAfterSeconds = 0) en MensajeDocument, MongoDB se encarga de la eliminación automática. Por lo tanto, no necesitamos una implementación manual en este adaptador para el requisito de caducidad.

---
+++ src/main/java/com/chat/hexagonal/domain/model/Reaccion.java
@@ -0,0 +1,16 @@
+// src/main/java/com/chat/hexagonal/domain/model/Reaccion.java
+package com.chat.hexagonal.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Reaccion {
+    private String userId;
+    private String emoji;
+}
